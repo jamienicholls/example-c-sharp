491e9d980d2b736e1a375721422160787a4a7f8d
Jamie Nicholls <J.Nicholls@massey.ac.nz>
2022-10-14 14:26:14 +1300

    Add project files.

---
+++ example-c-sharp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace example_c_sharp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
